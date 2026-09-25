b05131b7b0749a2ae67185f5bdf8c4561de7ee5a
jelinha <jelajelia@gmail.com>
2026-09-24 23:00:07 -0300

    Correção final

--- src/app/pages/alunas/alunas.ts
+++ src/app/pages/alunas/alunas.ts
@@ -1,18 +1,29 @@
-import { Component, computed, inject, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MockDataService } from '../../services/mock-data.service';
 
 @Component({
   selector: 'app-alunas',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './alunas.html',
   styleUrl: './alunas.css'
 })
-export class Alunas {
+export class Alunas implements OnInit {
   private readonly mockData = inject(MockDataService);
+  private readonly route = inject(ActivatedRoute);
 
-  readonly id = input<string>('');
+  protected readonly id = signal<string>('');
 
-  protected readonly alunaSelecionada = computed(() => this.mockData.perfisAlunas[this.id()] ?? null);
+  protected readonly alunaSelecionada = computed(() => {
+    const alunoId = this.id();
+    return this.mockData.perfisAlunas?.[alunoId] ?? null;
+  });
+
   protected readonly primeiroNome = computed(() => this.alunaSelecionada()?.nome.split(' ')[0] ?? '');
+
+  ngOnInit() {
+    const routeId = this.route.snapshot.paramMap.get('id') ?? '';
+    this.id.set(routeId);
+  }
 }
